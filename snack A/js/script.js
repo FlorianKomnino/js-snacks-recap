762726a46e9fff2fb11789ebb0305b9b1c926c12
FlorianKomnino <florian.komnino@outlook.it>
2022-12-22 16:02:27 +0100

    completed snack A, autoresponse with random sentence functioning with problems

--- snack A/js/script.js
+++ snack A/js/script.js
@@ -28,28 +28,6 @@ createApp ({
                         }
                     ],
                 },
-                {
-                    name: 'Fabio',
-                    avatar: '_2',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '20/03/2020 16:30:00',
-                            message: 'Ciao come stai?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '20/03/2020 16:30:55',
-                            message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
-                        },
-                        {
-                            date: '20/03/2020 16:35:00',
-                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
-                        }
-                    ],
-                },
 
             ],
 
@@ -59,31 +37,41 @@ createApp ({
 
             DateTime: luxon.DateTime,
 
+            randomSentenceFromResponse: '',
+
         }
     },
     
     methods : {
+
+        async getRandomSentence () {
+            await axios.get("https://flynn.boolean.careers/exercises/api/random/sentence")
+            .then((response) => {
+                this.randomSentenceFromResponse = response.data.response;
+                this.contacts[this.contactToShow].messages.push(
+                    {
+                        date: this.DateTime.now().toISO().replace('T', '  ').replaceAll('-', '/').slice(0, -10),
+                        message: this.messageToAdd,
+                        status: 'sent'
+                    });
+                    this.messageToAdd = '';
+                    setTimeout(this.receivedMessageAdder, 3000);
+            });
+        },
 
         messagesToShowFinder() {
             return this.contacts[this.contactToShow].messages;
         },
 
         messageAdderToConversation() {
-            this.contacts[this.contactToShow].messages.push(
-            {
-                date: this.DateTime.now().toISO().replace('T', '  ').replaceAll('-', '/').slice(0, -10),
-                message: this.messageToAdd,
-                status: 'sent'
-            });
-            this.messageToAdd = '';
-            setTimeout(this.receivedMessageAdder, 3000);
+
         },
 
         receivedMessageAdder() {
             this.contacts[this.contactToShow].messages.push(
                 {
                     date: this.DateTime.now().toISO().replace('T', '  ').replaceAll('-', '/').slice(0, -10),
-                    message: 'Ok! ;)',
+                    message: this.randomSentenceFromResponse,
                     status: 'received'
                 });
         },
