784eec70623f9f6e333d6527618255a2befa5156
FlorianKomnino <florian.komnino@outlook.it>
2022-12-22 16:40:18 +0100

    completed snack B, added axios call to get the array of generated numbers on the base of user input, linked to variable with v-model. Added subtitle to warn that numbers are unique and can't be more than 100

--- snack B/js/script.js
+++ snack B/js/script.js
@@ -5,7 +5,22 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Inserisci la quantità di numeri randomici, diversi tra loro, che vuoi generare.',
+
+            subtitle: 'non possono essere più di 100',
+
+            userRequestedQuantity: 0,
+
+            arrayOfRandomlyGeneratedUniqueNumbers : [],
         }
-    }
+    },
+
+    methods: {
+        async getRandomSequenceOfNumbers() {
+            await axios.get(`https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=${this.userRequestedQuantity}`)
+            .then((response) => {
+                this.arrayOfRandomlyGeneratedUniqueNumbers = response.data.response;
+            });
+        },
+    },
 }).mount ('#app')
